d07f391757c27bf0b16c08ee1a8eab33bc8ddf5d
pkyniklis <p.kyniklis@ihu.edu.gr>
2018-04-24 21:19:25 +0300

    added booked boolean

--- app/src/main/java/com/example/android/ticketing_2/GateActivity.java
+++ app/src/main/java/com/example/android/ticketing_2/GateActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -74,32 +75,36 @@ public class GateActivity extends AppCompatActivity {
         // see https://github.com/devsideal/VectorChildFinder
         final VectorChildFinder vector = new VectorChildFinder(this, frontID, frontImageView);
 
-        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Seats");
-        DatabaseReference mGateRef = mDatabase.child("Gate" + gateNo);
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Seats");
+        final DatabaseReference mGateRef = mDatabase.child("Gate" + gateNo);
         mGateRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Seat seat = dataSnapshot.getValue(Seat.class);
                 seatsList.add(seat);
-                if (!seat.isFree()) {
-                    String seatPathName = "r" + seat.getRow() + "s" + seat.getCol();
-                    final VectorDrawableCompat.VFullPath seatPath = vector.findPathByName(seatPathName);
-                    seatPath.setFillColor(0xff9e9e9e);
-                }
+
+                String seatPathName = "r" + seat.getRow() + "s" + seat.getCol();
+                final VectorDrawableCompat.VFullPath seatPath = vector.findPathByName(seatPathName);
+
+                if (seat.isFree() && seat.isBooked()) seatPath.setFillColor(0xfff9a825); //orange
+                if (seat.isFree() && !seat.isBooked()) seatPath.setFillColor(0xff64dd17); //green
+                if (!seat.isFree()) seatPath.setFillColor(0xff9e9e9e); //gray
+                
                 frontImageView.invalidate();
             }
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
                 Seat seat = dataSnapshot.getValue(Seat.class);
-                seatsList.add(seat);
+                //seatsList.add(seat);
+
                 String seatPathName = "r" + seat.getRow() + "s" + seat.getCol();
+                //seatsList.update(seatPathName, seat);
                 final VectorDrawableCompat.VFullPath seatPath = vector.findPathByName(seatPathName);
-                if (!seat.isFree()) {
-                    seatPath.setFillColor(0xff9e9e9e);
-                } else {
-                    seatPath.setFillColor(0xff64dd17);
-                }
+
+                if (seat.isFree() && seat.isBooked()) seatPath.setFillColor(0xfff9a825); //orange
+                if (seat.isFree() && !seat.isBooked()) seatPath.setFillColor(0xff64dd17); //green
+                if (!seat.isFree()) seatPath.setFillColor(0xff9e9e9e); //gray
                 frontImageView.invalidate();
             }
 
@@ -115,6 +120,7 @@ public class GateActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
             }
         });
+
 
         backImageView.setOnTouchListener(new View.OnTouchListener() {
             final int DOUBLE_TAP_DURATION = 500;
@@ -135,16 +141,40 @@ public class GateActivity extends AppCompatActivity {
                         int touchColor = hotspots.getPixel(x, y);
 
                         // Loop through all seats and find the one with the same color
+                        /*
                         for (Seat seat : seatsList) {
                             int seatColor = Color.parseColor(seat.getColor());
-                            if (touchColor == seatColor && seat.isFree()) {
+                            //if (touchColor == seatColor && seat.isFree()) {
+                            if (touchColor == seatColor) {
                                 // For this seat, do something (temporarily change color)
                                 String seatPathName = "r" + seat.getRow() + "s" + seat.getCol();
+                                seat.setFree(!seat.isFree());
+                                mGateRef.child(seatPathName).setValue(seat);
                                 final VectorDrawableCompat.VFullPath seatPath = vector.findPathByName(seatPathName);
-                                toggleColor(seatPath);
-                                frontImageView.invalidate();
+                                //toggleColor(seatPath);
+                                //frontImageView.invalidate();
                             }
                         }
+                        */
+                        for (Seat seat : seatsList) {
+                            int seatColor = Color.parseColor(seat.getColor());
+
+                            if (touchColor == seatColor && seat.isFree()) {
+
+                                //if ( !seat.isBooked() ) {
+                                if (seat.isFree()) {
+                                    Log.d("Petros", "add seat");
+                                    String seatPathName = "r" + seat.getRow() + "s" + seat.getCol();
+                                    //seat.setBooked(true);
+                                    seat.setBooked(!seat.isBooked());
+                                    mGateRef.child(seatPathName).setValue(seat);
+                                    //final VectorDrawableCompat.VFullPath seatPath = vector.findPathByName(seatPathName);
+                                    //setOrangeColor(seatPath);
+                                    //frontImageView.invalidate();
+                                }
+                            }
+                        }
+
                     }
                     tapTime = System.currentTimeMillis();
                 }
@@ -161,5 +191,9 @@ public class GateActivity extends AppCompatActivity {
     private void toggleColor(VectorDrawableCompat.VFullPath seat) {
         if (seat.getFillColor() != 0xff9e9e9e) seat.setFillColor(0xff9e9e9e);
         else seat.setFillColor(0xff64dd17);
+    }
+
+    private void setOrangeColor(VectorDrawableCompat.VFullPath seat) {
+        seat.setFillColor(0xfff9a825);
     }
 }
